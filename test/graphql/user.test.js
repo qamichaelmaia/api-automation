e667856e31c5256890eb017efeeb00cc122b6fe8
qamichaelmaia <contatomichaelmaia@gmail.com>
2024-04-09 17:28:20 -0300

    second commit

--- test/graphql/user.test.js
+++ test/graphql/user.test.js
@@ -1,9 +1,30 @@
 const { spec } = require('pactum');
 const { eachLike, like } = require('pactum-matchers');
 
+let token;
+
+beforeEach(async () => {
+  token = await spec()
+  .post('http://lojaebac.ebaconline.art.br/graphql')
+  .withGraphQLQuery(`
+    mutation Mutation($email: String, $password: String) {
+      authUser(email: $email, password: $password) {
+        success
+        token
+      }
+    }
+  `)
+  .withGraphQLVariables({
+      "email": "admin@admin.com",
+      "password": "admin123"
+  })
+  .returns('data.authUser.token')
+});
+
 it('Listagem de usuários', async () => {
   await spec()
     .post('http://lojaebac.ebaconline.art.br/graphql')
+    .withHeaders({"Authorization": token})
     .withGraphQLQuery(`
         query {
             Users {
@@ -19,10 +40,10 @@ it('Listagem de usuários', async () => {
     .expectJsonMatch({
         data: {
             Users: eachLike({
-                id: like("6601b8404cd5a2e84942488f"),
-                email: like("michaelqa@hotmail.com"),
+                id: like("65766e71ab7a6bdbcec70d0d"),
+                email: like("admin@admin.com"),
                 profile: {
-                    firstName: ("Michael")
+                    firstName: like("admin")
                 }
             })
         }
